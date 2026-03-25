037c3790190bcf27555f2d7803aefb3dca9860bc
andrewfunai <andrew.funai@gmail.com>
2026-03-25 02:18:49 -0600

    Update server.py

--- server.py
+++ server.py
@@ -299,11 +299,6 @@ def main():
 
 
 if __name__ == "__main__":
-    uvicorn.run(
-        "server:app", 
-        host="0.0.0.0", 
-        port=8443,
-        ssl_keyfile="key.pem",
-        ssl_certfile="cert.pem",
-        reload=False
-    )
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run("server:app", host="0.0.0.0", port=port)
